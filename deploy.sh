a703e99f9198d9700eb0cf5587f8fd584f1511d5
gethin.yan <gethin.yan@gmail.com>
2019-01-03 15:01:54 +0800

    add StrictHostKeyChecking=no

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install
 hexo g
-scp -r ./public/* root@47.52.175.26:/var/lib/docker/volumes/nginx-html/_data/blog/
+scp -o StrictHostKeyChecking=no -r ./public/* root@47.52.175.26:/var/lib/docker/volumes/nginx-html/_data/blog/
 echo 'travis build done!'
